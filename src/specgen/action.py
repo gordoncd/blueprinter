98eab52d16ac93ecb8f6c721ab43c2b823540415
Gordon Doore <gcdoor25@colby.edu>
2025-08-21 17:01:35 -0400

    drafting new vocab

--- src/specgen/action.py
+++ src/specgen/action.py
@@ -13,55 +13,42 @@ Last Modified: 2025-08-19
 from __future__ import annotations
 from dataclasses import dataclass
 from enum import Enum
-from typing import Any, Dict,Optional
+from typing import Any, Dict,Optional, List
 
 # ___________________________________________________
 # Operation Enums
 # ___________________________________________________
 
-class BusRole(str, Enum):
-    MAIN = "main"
-    SECTION = "section"
-    TIE = "tie"
-
-class Side(str, Enum):
-    HV = "HV"
-    LV = "LV"
-    TERT = "TERT"
-
-class Scheme(str, Enum):
-    SINGLE_BUSBAR = "single_busbar"
-    DOUBLE_BUSBAR = "double_busbar"
 
 class BayKind(str, Enum):
-    TRANSFORMER_BAY = "transformer_bay"
-    FEEDER_BAY = "feeder_bay"
-    LINE_BAY = "line_bay"
-    SHUNT_BAY = "shunt_bay"
+    TRANSFORMER = "TRANSFORMER"
+    FEEDER = "FEEDER"
+    LINE = "LINE"
+    SHUNT = "SHUNT"
+    COUPLER = "COUPLER"
+    GENERATOR = "GENERATOR"
 
-class StepKind(str, Enum):
-    BUS_ISOLATOR = "bus_isolator"
-    BREAKER = "breaker"
-    LINE_ISOLATOR = "line_isolator"
-    CT = "ct"
-    PT = "pt"
-    ARRESTER = "arrester"
-    PLACEHOLDER = "placeholder"
+class BreakerKind(str, Enum):
+    SF6 = "SF6"
+    VACUUM = "VACUUM"
+    OIL = "OIL"
+    AIRBLAST = "AIRBLAST"
+
+class DisconnectorKind(str, Enum):
+    CENTER_BREAK = "CENTER_BREAK"
+    DOUBLE_BREAK = "DOUBLE_BREAK"
+    PANTOGRAPH = "PANTOGRAPH"
+    EARTH_SWITCH_COMBINED = "EARTH_SWITCH_COMBINED"
 
 class TransformerKind(str, Enum):
-    THREE_WINDING = "three_winding"
-    TWO_WINDING = "two_winding"
-    AUTOTRANSFORMER = "autotransformer"
+    TWO_WINDING = "TWO_WINDING"
+    AUTO = "AUTO"
+    THREE_WINDING = "THREE_WINDING"
+    GROUNDING = "GROUNDING"
 
-class GroundingKind(str, Enum):
-    SOLID = "solid"
-    NGR = "ngr"  #neutral grounding resistor
-    REACTOR = "reactor"
-
-
-class ShuntKind(str, Enum):
-    CAPACITOR = "capacitor"
-    # REACTOR = "reactor"
+class LineKind(str, Enum):
+    OHL = "OHL"  # Overhead Line
+    UGC = "UGC"  # Underground Cable
 
 
 # ___________________________________________________
@@ -73,12 +60,15 @@ class ActionKind(str, Enum):
     Enum-like class to define action types for specgen model
     """
     ADD_BUS = "ADD_BUS"
-    ADD_COUPLER = "ADD_COUPLER"
-    ADD_TRANSFORMER = "ADD_TRANSFORMER"
     ADD_BAY = "ADD_BAY"
-    APPEND_STEP = "APPEND_STEP"
-    ADD_BUS_CONNECTION = "ADD_BUS_CONNECTION"
-    ADD_SHUNT = "ADD_SHUNT"
+    ADD_COUPLER = "ADD_COUPLER"
+    ADD_BREAKER = "ADD_BREAKER"
+    ADD_DISCONNECTOR = "ADD_DISCONNECTOR"
+    ADD_TRANSFORMER = "ADD_TRANSFORMER"
+    ADD_LINE = "ADD_LINE"
+    CONNECT = "CONNECT"
+    APPEND_TO_BAY = "APPEND_TO_BAY"
+    VALIDATE = "VALIDATE"
     EMIT_SPEC = "EMIT_SPEC"
 
 
@@ -89,64 +79,72 @@ class ActionKind(str, Enum):
 
 @dataclass
 class AddBusArgs:
-    diagram_id: int
+    diagram_id : str
     kv: float
-    role: BusRole
+
+@dataclass
+class AddBreakerArgs:
+    diagram_id: str
+    kv: float
+    interrupting_kv: float
+    kind: BreakerKind
+    continuous_A: float
 
 @dataclass
 class AddCouplerArgs:
-    diagram_id: int
+    diagram_id: str
     kv: float
-    busA_id: int
-    busB_id: int
+    from_bus: str
+    to_bus: str
 
 
 @dataclass
 class AddTransformerArgs:
-    diagram_id: int
+    diagram_id: str
     kind: TransformerKind
-    hv_kv: float
-    lv_kv: float
+    rated_mva: float
+    kv_in: float
+    kv_out: float
+    tert_kv: float
     rating_mva: float
-    tert_kv: Optional[float] = None  # Optional, only for three-winding transformers
-    vector_group: Optional[str] = None
-    impedance_percent: Optional[float] = None
+    vector_group: str
+    percentZ: float
     ref_mva: Optional[float] = None
-    grounding_hv: Optional[GroundingKind] = None
-    grounding_lv: Optional[GroundingKind] = None
-    grounding_tert: Optional[GroundingKind] = None
-
+    
 @dataclass
 class AddBayArgs:
-    diagram_id: int
-    side: Side
-    kv: float
+    diagram_id: str
     kind: BayKind
-    scheme: Optional[Scheme] = None
-    future: bool = False # Indicates if the bay is for future use
-
-@dataclass
-class AppendStepArgs:
-    bay_id: int
-    step_kind: StepKind
-
-@dataclass
-class AddBusConnectionArgs:
-    diagram_id: int
-    bus_id: int
-    element_id: int  # ID of transformer, bay, or shunt being connected
-    element_side: Optional[Side] = None  # Side of the element to connect to the bus
-
-@dataclass
-class AddShuntArgs:
-    diagram_id: int
-    kind: ShuntKind
     kv: float
-    mvar: float
-    bus_id: int
-    # steps_mvar: Optional[List[Tuple[StepKind, float]]] = None # List of (step_kind, mvar) tuples for shunt steps
-    banks: Optional[int] = None  # Number of banks for the shunt, if applicable
+    bus: str
+
+@dataclass
+class AppendToBayArgs:
+    diagram_id: str
+    object_id: str 
+
+@dataclass
+class ConnectArgs:
+    series: List[str] #list of IDs
+
+@dataclass
+class AddLineArgs:
+    diagram_id: int
+    kv: float
+    kind : LineKind
+    length_km: float
+    thermal_A: float
     
+@dataclass
+class AddDisconnectorArgs:
+    diagram_id: str
+    kv: float
+    kind: DisconnectorKind
+    continuous_A: float
+
+@dataclass
+class ValidateArgs:
+    pass
 
 @dataclass
 class EmitSpecArgs:
@@ -169,9 +167,11 @@ ACTION_ARG_SCHEMA: Dict[ActionKind, Any] = {
     ActionKind.ADD_COUPLER: AddCouplerArgs,
     ActionKind.ADD_TRANSFORMER: AddTransformerArgs,
     ActionKind.ADD_BAY: AddBayArgs,
-    ActionKind.APPEND_STEP: AppendStepArgs,
-    ActionKind.ADD_BUS_CONNECTION: AddBusConnectionArgs,
-    ActionKind.ADD_SHUNT: AddShuntArgs,
+    ActionKind.APPEND_TO_BAY: AppendToBayArgs,
+    ActionKind.CONNECT: ConnectArgs,
+    ActionKind.ADD_LINE: AddLineArgs,
+    ActionKind.ADD_DISCONNECTOR: AddDisconnectorArgs,
+    ActionKind.VALIDATE: ValidateArgs,
     ActionKind.EMIT_SPEC: EmitSpecArgs,
 }
 
